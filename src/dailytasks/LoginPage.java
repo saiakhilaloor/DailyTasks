b92f0e9436d48f7f5ad04bfc45132d6cf667b7d4
Sai Akhil Aloor <saiakhil.aloor7@gmail.com>
2017-09-16 16:06:44 +0530

    Update LoginPage.java

--- src/dailytasks/LoginPage.java
+++ src/dailytasks/LoginPage.java
@@ -72,7 +72,7 @@ public class LoginPage {
             public void actionPerformed(ActionEvent e){
                 try{
                     Class.forName("com.mysql.jdbc.Driver");
-                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost/dailytasks", "root", "console.log()");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost/dailytasks", "root", "");
                     Statement st = con.createStatement();
                     ResultSet rs;
                     String query;
